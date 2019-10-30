0d0cfe7c3c66d4c6e4cfd9c7c4ad0b581e4dfb88
addeemaulana <addemaulana@gmail.com>
2019-10-30 09:28:49 +0700

    Koreksi route profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 
-const ProfileModel = require('../../models/Profile')
-const UserModel = require('../../models/User')
+const ProfileModel = require("../../models/Profile");
+const UserModel = require("../../models/User");
 
 // @route     GET api/profile/me
 // @desc      Get current users profile
 // @access    Private
 router.get("/", auth, async (req, res) => {
-    try {
-        const profile = await ProfileModel.findOne({user: req.user.id })
-    } catch (err) {
-        console.error(err.message)
-        res.status(500).send('Server Error')
-    }
-}
+  try {
+    const profile = await ProfileModel.findOne({ user: req.user.id });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
