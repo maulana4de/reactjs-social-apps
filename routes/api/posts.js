1c66b312ebf498bd4a8504546f0e7682c44b9066
addeemaulana <addemaulana@gmail.com>
2019-11-05 16:46:00 +0700

    Update posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,80 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route     GET api/posts
-// @desc      Test route for posts
-// @access    Public
-router.get('/', (req, res) => res.send('posts route is running'));
+const ProfileModel = require("../../models/Profile");
+const UserModel = require("../../models/User");
+const PostModel = require("../../models/Post");
+
+// @route     Post api/posts
+// @desc      Create a posts
+// @access    Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const user = await UserModel.findById(req.user.id).select("-password");
+      const newPost = new PostModel({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route     Get api/posts
+// @desc      Get all posts
+// @access    Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await PostModel.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// @route     Get api/posts/:id
+// @desc      Get post by ID
+// @access    Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
